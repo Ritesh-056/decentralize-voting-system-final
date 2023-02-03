1cc54c44b2e71a76dcd25208d2cc5b1942cba2cb
Ritesh-056 <baralriteshrtz@gmail.com>
2023-02-03 12:34:44 +0545

    project modified

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-
-// Dummy ===================================================
-// import App from "./dummy/App";
-// import * as serviceWorker from "./dummy/serviceWorker";
-// import reducer from "./dummy/store/reducer";
-// import config from "./dummy/config";
-
-// const store = createStore(reducer);
-
-// const app = (
-//   <Provider store={store}>
-//     <BrowserRouter basename={config.basename}>
-//       {/* basename="/datta-able" */}
-//       <App />
-//     </BrowserRouter>
-//   </Provider>
-// );
-
-// ReactDOM.render(app, document.getElementById("root"));
-
-// serviceWorker.unregister();
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-
-// Main =====================================================
 import App from "./App";
+import ReactDOM from 'react-dom';
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,91 +1,182 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
 
 contract Election {
-    address public owner;
-    uint public candidateCount;
-    uint public electionCount;
-    uint public voteCount;
+    address public admin;
+    uint256 candidateCount;
+    uint256 voterCount;
+    bool start;
+    bool end;
 
-    mapping(uint => Candidate) public candidates;
-    mapping(address => bool) public voters;
-    mapping(uint => CreateElection) public elections;
+    constructor()  {
+        // Initilizing default values
+        admin = msg.sender;
+        candidateCount = 0;
+        voterCount = 0;
+        start = false;
+        end = false;
+    }
 
-    event NewCreateElection(uint electionID, string name, string department);
-    event NewCandidate(uint candidateID, string name, string department);
-    event NewVote(uint candidateID);
+    function getAdmin() public view returns (address) {
+        // Returns account address used to deploy contract (i.e. admin)
+        return admin;
+    }
 
+    modifier onlyAdmin() {
+        // Modifier for only admin access
+        require(msg.sender == admin);
+        _;
+    }
+    // Modeling a candidate
     struct Candidate {
+        uint256 candidateId;
+        string header;
+        string slogan;
+        uint256 voteCount;
+    }
+    mapping(uint256 => Candidate) public candidateDetails;
+
+    // Adding new candidates
+    function addCandidate(string memory _header, string memory _slogan)
+        public
+        // Only admin can add
+        onlyAdmin
+    {
+        Candidate memory newCandidate =
+            Candidate({
+                candidateId: candidateCount,
+                header: _header,
+                slogan: _slogan,
+                voteCount: 0
+            });
+        candidateDetails[candidateCount] = newCandidate;
+        candidateCount += 1;
+    }
+
+    // Modeling a Election Details
+    struct ElectionDetails {
+        string adminName;
+        string adminEmail;
+        string adminTitle;
+        string electionTitle;
+        string organizationTitle;
+    }
+    ElectionDetails electionDetails;
+
+    function setElectionDetails(
+        string memory _adminName,
+        string memory _adminEmail,
+        string memory _adminTitle,
+        string memory _electionTitle,
+        string memory _organizationTitle
+    )
+        public
+        // Only admin can add
+        onlyAdmin
+    {
+        electionDetails = ElectionDetails(
+            _adminName,
+            _adminEmail,
+            _adminTitle,
+            _electionTitle,
+            _organizationTitle
+        );
+        start = true;
+        end = false;
+    }
+
+    // Get Elections details
+    function getAdminName() public view returns (string memory) {
+        return electionDetails.adminName;
+    }
+
+    function getAdminEmail() public view returns (string memory) {
+        return electionDetails.adminEmail;
+    }
+
+    function getAdminTitle() public view returns (string memory) {
+        return electionDetails.adminTitle;
+    }
+
+    function getElectionTitle() public view returns (string memory) {
+        return electionDetails.electionTitle;
+    }
+
+    function getOrganizationTitle() public view returns (string memory) {
+        return electionDetails.organizationTitle;
+    }
+
+    // Get candidates count
+    function getTotalCandidate() public view returns (uint256) {
+        // Returns total number of candidates
+        return candidateCount;
+    }
+
+    // Get voters count
+    function getTotalVoter() public view returns (uint256) {
+        // Returns total number of voters
+        return voterCount;
+    }
+
+    // Modeling a voter
+    struct Voter {
+        address voterAddress;
         string name;
-        string department;
-        uint voteCount;
+        string phone;
+        bool isVerified;
+        bool hasVoted;
+        bool isRegistered;
+    }
+    address[] public voters; // Array of address to store address of voters
+    mapping(address => Voter) public voterDetails;
+
+    // Request to be added as voter
+    function registerAsVoter(string memory _name, string memory _phone) public {
+        Voter memory newVoter =
+            Voter({
+                voterAddress: msg.sender,
+                name: _name,
+                phone: _phone,
+                hasVoted: false,
+                isVerified: false,
+                isRegistered: true
+            });
+        voterDetails[msg.sender] = newVoter;
+        voters.push(msg.sender);
+        voterCount += 1;
     }
 
-    struct CreateElection {
-        uint electionId;
-        string name;
-        string department;
-        uint electionDate;
+    // Verify voter
+    function verifyVoter(bool _verifedStatus, address voterAddress)
+        public
+        // Only admin can verify
+        onlyAdmin
+    {
+        voterDetails[voterAddress].isVerified = _verifedStatus;
     }
 
-    constructor() {
-        owner = msg.sender;
+    // Vote
+    function vote(uint256 candidateId) public {
+        require(voterDetails[msg.sender].hasVoted == false);
+        require(voterDetails[msg.sender].isVerified == true);
+        require(start == true);
+        require(end == false);
+        candidateDetails[candidateId].voteCount += 1;
+        voterDetails[msg.sender].hasVoted = true;
     }
 
-
-    function createElection(
-        string memory _electionName,
-        string memory _electionDepartment,
-        uint _electionDate
-    ) public {
-        require(
-            msg.sender == owner,
-            "Only the contract owner can create election"
-        );
-        electionCount++;
-        elections[electionCount] = CreateElection(
-            electionCount,
-            _electionName,
-            _electionDepartment,
-            _electionDate
-        );
-        emit NewCandidate(electionCount, _electionName, _electionDepartment);
+    // End election
+    function endElection() public onlyAdmin {
+        end = true;
+        start = false;
     }
 
-    function addCandidate(
-        string memory _name,
-        string memory _department
-    ) public {
-        require(
-            msg.sender == owner,
-            "Only the contract owner can add candidates."
-        );
-        candidateCount++;
-        candidates[candidateCount] = Candidate(_name, _department, 0);
-        emit NewCandidate(candidateCount, _name, _department);
+    // Get election start and end values
+    function getStart() public view returns (bool) {
+        return start;
     }
 
-    function vote(uint _candidateID) public {
-        require(!voters[msg.sender], "You have already voted.");
-        require(
-            _candidateID > 0 && _candidateID <= candidateCount,
-            "Invalid candidate ID."
-        );
-        voters[msg.sender] = true;
-        candidates[_candidateID].voteCount++;
-        voteCount++;
-        emit NewVote(_candidateID);
-    }
-
-    function winner() public view returns (uint) {
-        uint winningCandidateID = 0;
-        uint winningVoteCount = 0;
-        for (uint i = 1; i <= candidateCount; i++) {
-            if (candidates[i].voteCount > winningVoteCount) {
-                winningCandidateID = i;
-                winningVoteCount = candidates[i].voteCount;
-            }
-        }
-        return winningCandidateID;
+    function getEnd() public view returns (bool) {
+        return end;
     }
 }
