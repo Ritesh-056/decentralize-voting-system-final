83ed0f9fd83f12030236f24e1c36003aed2896c3
Roman-Ojha <razzroman98@gmail.com>
2023-02-08 16:55:59 +0545

    fixed contract network id problem

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,19 +3,18 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.1",
-  // networks: {
-  //   hardhat: {
-  //     chainId: 1337,
-  //   },
-  //   ganache: {
-  //     url: "http://127.0.0.1:8545",
-  //     accounts: [
-  //       `d7b29d2317b6362af1290f397c8705d1c775036a615071d28e126e48f6d65c50`,
-  //     ],
-  //   }
   networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+    },
+    // ganache: {
+    //   url: "http://127.0.0.1:8545",
+    //   accounts: [
+    //     `d7b29d2317b6362af1290f397c8705d1c775036a615071d28e126e48f6d65c50`,
+    //   ],
+    // },
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
   },
   paths: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,9 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
+const { ethers } = require("hardhat");
 
 async function main() {
-  // We get the contract to deploy
-  const ElectionFactory = await hre.ethers.getContractFactory("Election");
-  const election = await ElectionFactory.deploy();
-
-  await election.deployed();
-
-  console.log("Election deployed to:", election.address);
+  const Election = await ethers.getContractFactory("Election");
+  const election = await Election.deploy();
+  console.log("Contract address: ", election.address);
 }
 
 main()
@@ -23,5 +12,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-
